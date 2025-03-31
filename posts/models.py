b5a77932f3a0a1e3bba13695b447367d218be50e
yunseo <jysjys2669@gmail.com>
2025-03-31 09:57:43 +0900

    posts modeling, migrate

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    image = models.ImageField(upload_to = 'image') # image 폴더 생성 후 그 안에 이미지 저장됨 / pillow 설치 ! 
